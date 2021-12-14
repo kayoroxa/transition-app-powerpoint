7164222f8d35144798f4aaf2f351302e8c572d27
Caio <kayoroxa@gmail.com>
2021-12-14 18:00:32 -0300

    template fix

--- templates/_templateTeachAndExample.js
+++ templates/_templateTeachAndExample.js
@@ -9,8 +9,8 @@ function _templateTeachAndExample(params) {
         title: params.title,
         description: params.description,
       }).caixa({
-        width: relativeHorizontal(70),
-        height: relativeVertical(40),
+        width: relativeHorizontal(70), // 70% of the screen width
+        height: relativeVertical(40), // 40% of the screen height
       }),
       createElement('exemple', {
         id: '',
@@ -27,7 +27,7 @@ function _templateTeachAndExample(params) {
           l['teach'].flex({ w: 7, h: 4 })
         },
         () => {
-          l['teach'].caixa({ opacity: 0, top: '150vh' }).flex() //.caixa({ top: '-50vh' })
+          l['teach'].caixa({ opacity: 0, top: '150vh' }).flex() // hide and remove flex caixa
           l['exemple'].flex({ w: 4, h: 2 })
           l['img'].flex({ w: 7, h: 7 })
         },
