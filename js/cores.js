2e612f1873bf95b524de00f8988d226cca0af29a
Caio <kayoroxa@gmail.com>
2021-12-14 12:46:21 -0300

    cores

---
+++ js/cores.js
@@ -0,0 +1,17 @@
+function Cores() {
+  return {
+    gray: '#404040',
+    black: '#262626',
+    yellow: '#f7f707',
+    orange: '#fb5607',
+    pink: '#ff006e',
+    red: '#ff0000',
+    green: '#8ac926',
+    blue: '#3a86ff',
+    purple: '#8338ec',
+    white: '#ffffff',
+  }
+}
+
+const cor = Cores()
+const color = cor.gray
