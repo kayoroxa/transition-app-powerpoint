bc87100cfa5f15ae560a1d88567ce6965be93cb7
Caio <kayoroxa@gmail.com>
2021-12-14 17:27:55 -0300

    catalogation

--- js/catalogation.js
+++ js/catalogation.js
@@ -1,15 +1,15 @@
-// const _3Examples = _template3Examples({
-//   images: [
-//     'https://picsum.photos/700/700',
-//     'https://picsum.photos/700/700',
-//     'https://picsum.photos/700/700',
-//   ],
-//   exemples: [
-//     ["hi i'ts (me)", 'olá eu sou (eu)'],
-//     ['you gonna tell to (me)', 'você vai dizer para (mim)'],
-//     ['take (me) away from this', '(me) tire disso'],
-//   ],
-// })
+const have3Examples = _template3Examples({
+  images: [
+    'https://picsum.photos/700/700',
+    'https://picsum.photos/700/700',
+    'https://picsum.photos/700/700',
+  ],
+  exemples: [
+    ["hi i'ts (me)", 'olá eu sou (eu)'],
+    ['you gonna tell to (me)', 'você vai dizer para (mim)'],
+    ['take (me) away from this', '(me) tire disso'],
+  ],
+})
 
 const make = _templateTeachAndExample({
   title: 'Have = Ter',
@@ -17,11 +17,11 @@ const make = _templateTeachAndExample({
   exemple: ['olá eu sou eu', 'olá eu sou eu'],
   image: 'https://picsum.photos/700/700',
 })
-const make2 = _templateTeachAndExample({
+const have = _templateTeachAndExample({
   title: 'to do = Ter',
   description: 'to do que você tem algo',
   exemple: ['oto dou eu', 'olá eto do eu'],
   image: 'https://picsum.photos/700/700',
 })
 
-generateCena([make, make2])
+generateCena([make, have, have3Examples])
