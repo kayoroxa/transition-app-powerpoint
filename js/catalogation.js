0ed4a51e0d814ad4e9dfc21e2711db2967a599be
Caio <kayoroxa@gmail.com>
2021-12-14 23:36:07 -0300

    catalogation

--- js/catalogation.js
+++ js/catalogation.js
@@ -1,15 +1,15 @@
-const have3Examples = _template3Examples({
-  images: [
-    'https://picsum.photos/700/700',
-    'https://picsum.photos/700/700',
-    'https://picsum.photos/700/700',
-  ],
-  exemples: [
-    ["hi i'ts (me)", 'olá eu sou (eu)'],
-    ['you gonna tell to (me)', 'você vai dizer para (mim)'],
-    ['take (me) away from this', '(me) tire disso'],
-  ],
-})
+// const have3Examples = _template3Examples({
+//   images: [
+//     'https://picsum.photos/700/700',
+//     'https://picsum.photos/700/700',
+//     'https://picsum.photos/700/700',
+//   ],
+//   exemples: [
+//     ["hi i'ts (me)", 'olá eu sou (eu)'],
+//     ['you gonna tell to (me)', 'você vai dizer para (mim)'],
+//     ['take (me) away from this', '(me) tire disso'],
+//   ],
+// })
 
 const make = _templateTeachAndExample({
   title: 'Have = Ter',
@@ -17,11 +17,22 @@ const make = _templateTeachAndExample({
   exemple: ['olá eu sou eu', 'olá eu sou eu'],
   image: 'https://picsum.photos/700/700',
 })
-const have = _templateTeachAndExample({
-  title: 'to do = Ter',
-  description: 'to do que você tem algo',
-  exemple: ['oto dou eu', 'olá eto do eu'],
-  image: 'https://picsum.photos/700/700',
-})
+// const have = _templateTeachAndExample({
+//   title: 'to do = Ter',
+//   description: 'to do que você tem algo',
+//   exemple: ['oto dou eu', 'olá eto do eu'],
+//   image: 'https://picsum.photos/700/700',
+// })
 
-generateCena([make, have, have3Examples])
+const spell = _3Imagens({
+  headers: ['spell = feitiço', 'spell = soletrar'],
+  colors: ['pink', 'green'],
+  from: 'bottom',
+})
+generateCena([
+  //
+  spell,
+  // make,
+  // have,
+  // have3Examples,
+])
